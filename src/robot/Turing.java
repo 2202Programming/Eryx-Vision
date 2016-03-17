311bb5e01d360453f61266eb26b564ef51e95dd2
leinad2018 <leinad2018@gmail.com>
2016-03-17 16:17:55 -0500

    Added distance calc to vision

--- src/robot/Turing.java
+++ src/robot/Turing.java
@@ -4,25 +4,29 @@ package robot;
 import java.awt.Point;
 
 public class Turing {
-	private static float fieldOfView=47.15f;
-	private static final float heightToTopOfUFromGround=7, heightOfCameraFromGround=2, degreesOfBottomOfFOV=10; 
-	
+	private static float fieldOfView = 47.15f;
+	private static final float heightToTopOfUFromGround = 96, heightOfCameraFromGround = 17, degreesOfBottomOfFOV = 42;
+
 	/**
 	 * Gets the number of degrees to turn to the right.
+	 * 
 	 * @param width
-	 * The width of the screen
+	 *            The width of the screen
 	 * @param center
-	 * The point representing the center of the goal
-	 * @return
-	 * The number of degrees to turn to the right.
+	 *            The point representing the center of the goal
+	 * @return The number of degrees to turn to the right.
 	 */
 	public static float getDegreesToTurn(float width, Point center) {
-		return fieldOfView*(center.x/width-0.5f);
+		return fieldOfView * (center.x / width - 0.5f);
 	}
-	
+
 	public static double getDistanceFromGoal(float width, float height, Point center) {
-		float yFOV=fieldOfView*height/width;
-		float beta=yFOV*((height-center.y)/height);
-		return (heightToTopOfUFromGround-heightOfCameraFromGround)/(Math.atan(degreesOfBottomOfFOV+beta));
+		float yFOV = fieldOfView * height / width;
+		float x = height - (height - center.y);
+		float z = yFOV * (x / height);
+		float y = degreesOfBottomOfFOV - (0.5f * yFOV);
+		float q = y + z;
+		//float q = degreesOfBottomOfFOV - (0.5f * yFOV) + yFOV * (height - (height - center.y) / height);
+		return ((heightToTopOfUFromGround - heightOfCameraFromGround) / Math.tan(q));
 	}
 }
